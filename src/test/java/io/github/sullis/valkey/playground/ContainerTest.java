c9fa6f634a5b2fe605f1a2097a2067b9cdd89a4e
sullis <github@seansullivan.com>
2024-07-10 13:38:10 -0700

    valkey cluster (#3)

--- src/test/java/io/github/sullis/valkey/playground/ContainerTest.java
+++ src/test/java/io/github/sullis/valkey/playground/ContainerTest.java
@@ -1,9 +1,15 @@
 package io.github.sullis.valkey.playground;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.utility.DockerImageName;
 import glide.api.GlideClient;
 import glide.api.models.configuration.GlideClientConfiguration;
@@ -13,23 +19,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ContainerTest {
-  private static final GenericContainer container = new GenericContainer(DockerImageName.parse("valkey/valkey")).withExposedPorts(6379);
+  private static final Logger LOGGER = LoggerFactory.getLogger(ContainerTest.class);
+  private static final int CLUSTER_SIZE = 3;
+  private static final List<GenericContainer> containers = createValkeyCluster(CLUSTER_SIZE, 6379);
+
+  private static List<GenericContainer> createValkeyCluster(final int n, final int basePort) {
+    Network network = Network.newNetwork();
+    GenericContainer primary = null;
+    List<GenericContainer> cluster = new ArrayList<>();
+    int port = basePort;
+    for (int i = 0; i < n; i++) {
+      StringBuilder command = new StringBuilder();
+      command.append("valkey-server --port " + port);
+      GenericContainer container = new GenericContainer(DockerImageName.parse("valkey/valkey"))
+          .withNetwork(network)
+          .withExposedPorts(port)
+          .withLogConsumer(new Slf4jLogConsumer(LOGGER));
+      if (i == 0) {
+        primary = container;
+      } else {
+        command.append(" --replicaof " + primary.getHost() + " " + primary.getFirstMappedPort());
+      }
+      container = container.withCommand(command.toString());
+      cluster.add(container);
+      container.start();
+      port++;
+    }
+    return cluster;
+  }
+
+  private static void logStatus(final GenericContainer container) {
+    LOGGER.info("container " + container.getContainerId() + " isRunning=" + container.isRunning());
+  }
 
   @BeforeAll
   static void beforeAll() {
-    container.start();
+    containers.forEach(ContainerTest::logStatus);
   }
 
   @AfterAll
   static void afterAll() {
-    container.stop();
+    containers.forEach(GenericContainer::stop);
   }
 
   @Test
   void testValkeyClient() throws Exception {
-    NodeAddress address =  NodeAddress.builder().host(container.getHost()).port(container.getFirstMappedPort()).build();
-    GlideClientConfiguration config = GlideClientConfiguration.builder().address(address).build();
-    GlideClient client = GlideClient.createClient(config).get();
+    GlideClientConfiguration.GlideClientConfigurationBuilder configBuilder = GlideClientConfiguration.builder();
+    containers.forEach(c -> {
+      NodeAddress address =  NodeAddress.builder().host(c.getHost()).port(c.getFirstMappedPort()).build();
+      LOGGER.info("NodeAddress: " + address.getHost() + ":" + address.getPort());
+      configBuilder.address(address);
+    });
+    GlideClient client = GlideClient.createClient(configBuilder.build()).get();
     assertThat(client.ping("Hello world").get()).isEqualTo("Hello world");
+
+    String clientInfo = client.info().get();
+    assertThat(clientInfo)
+        .contains("connected_clients:1")
+        .contains("server_name:valkey")
+        .contains("role:master");
+
+    // LOGGER.info("client.info: " + clientInfo);
   }
 }
