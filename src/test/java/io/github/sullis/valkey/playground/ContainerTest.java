c38621bfa7cbb3bc6bbe5c6281b6d1f24e9276f6
sullis <github@seansullivan.com>
2026-09-15 16:29:33 -0400

    valkey container 9.1.2

--- src/test/java/io/github/sullis/valkey/playground/ContainerTest.java
+++ src/test/java/io/github/sullis/valkey/playground/ContainerTest.java
@@ -36,7 +36,7 @@ public class ContainerTest {
     for (int i = 0; i < numContainers; i++) {
       StringBuilder command = new StringBuilder();
       command.append("valkey-server --port " + port);
-      GenericContainer<?> container = new GenericContainer(DockerImageName.parse("valkey/valkey:8.1.1"))
+      GenericContainer<?> container = new GenericContainer(DockerImageName.parse("valkey/valkey:9.1.2"))
           .withNetwork(network)
           .withExposedPorts(port)
           .withLogConsumer(new Slf4jLogConsumer(LOGGER));
